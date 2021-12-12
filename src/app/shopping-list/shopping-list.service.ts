48d35bc892ba7a47f8d41bd6230dae1b2bc3875f
Glenn Pinto <gvpinto@gmail.com>
2021-12-12 18:36:33 -0500

    Using Services for Pushing Data from A to B

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -1,6 +1,8 @@
+import { EventEmitter } from '@angular/core';
 import { Ingredient } from '../shared/ingredient.mode';
 
 export class ShoppingListService {
+  ingredientsChanged = new EventEmitter<Ingredient[]>();
   private ingredients: Ingredient[] = [
     new Ingredient('Carrots', 4),
     new Ingredient('Tomotoes', 9),
@@ -12,5 +14,6 @@ export class ShoppingListService {
 
   addIngredient(ingredient: Ingredient): void {
     this.ingredients.push(ingredient);
+    this.ingredientsChanged.emit(this.ingredients.slice());
   }
 }
