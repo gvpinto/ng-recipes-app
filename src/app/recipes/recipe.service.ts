be52241d7e5a6a69f905d78d336d51d25f76a320
Glenn Pinto <gvpinto@gmail.com>
2021-12-18 09:46:45 -0500

    123. Passing Ingredients from Recipes to the Shopping List (via a Service) - My Solution

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,4 +1,5 @@
 import { EventEmitter } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.mode';
 import { Recipe } from './recipe-model';
 
 export class RecipeService {
@@ -7,12 +8,14 @@ export class RecipeService {
     new Recipe(
       'A Test Recipe',
       'This is simply a test',
-      'https://www.cookingclassy.com/wp-content/uploads/2021/10/beef-stew-30.jpg'
+      'https://www.cookingclassy.com/wp-content/uploads/2021/10/beef-stew-30.jpg',
+      [new Ingredient('Meat', 1), new Ingredient('Bread', 3)]
     ),
     new Recipe(
       'Another Test Recipe',
       'This is simply another test',
-      'https://www.cookingclassy.com/wp-content/uploads/2021/10/beef-stew-30.jpg'
+      'https://www.cookingclassy.com/wp-content/uploads/2021/10/beef-stew-30.jpg',
+      [new Ingredient('Chicken', 2), new Ingredient('Buns', 5)]
     ),
   ];
 
--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -16,4 +16,8 @@ export class ShoppingListService {
     this.ingredients.push(ingredient);
     this.ingredientsChanged.emit(this.ingredients.slice());
   }
+  addIngredients(ingredients: Ingredient[]): void {
+    this.ingredients.push(...ingredients);
+    this.ingredientsChanged.emit(this.ingredients.slice());
+  }
 }
