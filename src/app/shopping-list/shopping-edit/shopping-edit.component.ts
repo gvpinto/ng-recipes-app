9b716aec0f4ec71f47b66159983d10fb9c0b8516
Glenn Pinto <gvpinto@gmail.com>
2021-12-29 16:42:24 -0500

    222. Loading the Shopping List Items into the Form

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -17,9 +17,11 @@ import { ShoppingListService } from '../shopping-list.service';
   styleUrls: ['./shopping-edit.component.css'],
 })
 export class ShoppingEditComponent implements OnInit {
+  @ViewChild('f', { static: false }) shoppingListForm?: NgForm;
   subscription?: Subscription;
   editMode = false;
   editedItemIndex?: number;
+  editedItem?: Ingredient;
 
   constructor(private shoppingListService: ShoppingListService) {}
 
@@ -28,6 +30,11 @@ export class ShoppingEditComponent implements OnInit {
       (index: number) => {
         this.editMode = true;
         this.editedItemIndex = index;
+        this.editedItem = this.shoppingListService.getIngredient(index);
+        this.shoppingListForm!.setValue({
+          name: this.editedItem.name,
+          amount: this.editedItem.amount,
+        });
       }
     );
   }
