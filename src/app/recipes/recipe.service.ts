6cdaa569b2b9921830b8ed515fa3e2247fc7dd4e
Glenn Pinto <gvpinto@gmail.com>
2022-01-08 16:24:31 -0500

    284. Transforming Response Data

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -8,20 +8,21 @@ import { Recipe } from './recipe-model';
 export class RecipeService {
   recipeChanged = new Subject<Recipe[]>();
 
-  private recipes: Recipe[] = [
-    new Recipe(
-      'Hamburger',
-      'Declious Hamburger',
-      'https://img.taste.com.au/dkEhBL4s/w643-h428-cfill-q90/taste/2016/11/hamburger-with-caramelised-pineapple-90338-1.jpeg',
-      [new Ingredient('Meat', 1), new Ingredient('Bread', 3)]
-    ),
-    new Recipe(
-      'Beef Stew',
-      'Yummy Stew!',
-      'https://img.taste.com.au/uEWtNSPx/w643-h428-cfill-q90/taste/2019/07/classic-beef-stew-151457-2.jpg',
-      [new Ingredient('Chicken', 2), new Ingredient('Buns', 5)]
-    ),
-  ];
+  private recipes: Recipe[] = [];
+  //   private recipes: Recipe[] = [
+  //     new Recipe(
+  //       'Hamburger',
+  //       'Declious Hamburger',
+  //       'https://img.taste.com.au/dkEhBL4s/w643-h428-cfill-q90/taste/2016/11/hamburger-with-caramelised-pineapple-90338-1.jpeg',
+  //       [new Ingredient('Meat', 1), new Ingredient('Bread', 3)]
+  //     ),
+  //     new Recipe(
+  //       'Beef Stew',
+  //       'Yummy Stew!',
+  //       'https://img.taste.com.au/uEWtNSPx/w643-h428-cfill-q90/taste/2019/07/classic-beef-stew-151457-2.jpg',
+  //       [new Ingredient('Chicken', 2), new Ingredient('Buns', 5)]
+  //     ),
+  //   ];
 
   constructor(private shoppingListService: ShoppingListService) {}
 
--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { map } from 'rxjs/operators';
 import { Recipe } from '../recipes/recipe-model';
 import { RecipeService } from '../recipes/recipe.service';
 
@@ -24,6 +25,16 @@ export class DataStorageService {
     this.http
       .get<Recipe[]>(
         'https://ng-ps-recipe-app-default-rtdb.firebaseio.com/recipes.json'
+      )
+      .pipe(
+        map((recipes) => {
+          return recipes.map((recipe) => {
+            return {
+              ...recipe,
+              ingredients: recipe.ingredients ? recipe.ingredients : [],
+            };
+          });
+        })
       )
       .subscribe((recipes) => {
         this.recipeService.setRecipes(recipes);
