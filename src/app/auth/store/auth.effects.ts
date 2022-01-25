65f0a35caadbf30c85773cd690241d7cefbc81db
Glenn Pinto <gvpinto@gmail.com>
2022-01-24 22:01:02 -0500

    371. Finishing the Login Effect

--- src/app/auth/store/auth.effects.ts
+++ src/app/auth/store/auth.effects.ts
@@ -1,7 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
 import { Actions, Effect, ofType } from '@ngrx/effects';
-import { catchError, map, of, switchMap } from 'rxjs';
+import { catchError, map, of, switchMap, tap } from 'rxjs';
 import { AuthKeyService } from '../auth-key.service';
 import * as AuthActions from '../store/auth.actions';
 
@@ -16,8 +17,8 @@ export interface AuthResponseData {
 }
 @Injectable()
 export class AuthEffects {
-  @Effect()
   // This effect observable should never die
+  @Effect()
   authLogin = this.actions$.pipe(
     ofType(AuthActions.LOGIN_START),
     // Creates a new Observable
@@ -32,30 +33,60 @@ export class AuthEffects {
           }
         )
         .pipe(
+          // map automatically returns an observable
           map((resData) => {
             // + sign to convert to nummeric
             const expirationDate = new Date(
               new Date().getTime() + +resData.expiresIn * 1000
             );
-            return of(
-              new AuthActions.Login({
-                email: resData.email,
-                userId: resData.localId,
-                token: resData.idToken,
-                expirationDate: expirationDate,
-              })
-            );
+
+            return new AuthActions.Login({
+              email: resData.email,
+              userId: resData.localId,
+              token: resData.idToken,
+              expirationDate: expirationDate,
+            });
           }),
-          catchError((error) => {
+          catchError((errorResp) => {
             // Should return a non error Observable so the action$.pop Observable does not die.
-            return of();
+            let errorMessage = 'An Unknown Error Occurred';
+            if (!errorResp.error || !errorResp.error.error) {
+              return of(new AuthActions.LoginFail(errorMessage));
+            }
+            switch (errorResp.error.error.message) {
+              case 'EMAIL_EXISTS':
+                errorMessage = 'This email already exists';
+                break;
+              case 'EMAIL_NOT_FOUND':
+                errorMessage = 'This email does not exists';
+                break;
+              case 'INVALID_PASSWORD':
+                errorMessage = 'Password is invalid';
+                break;
+              case 'TOO_MANY_ATTEMPTS_TRY_LATER':
+                errorMessage = 'Too many attempts';
+                break;
+              default:
+                break;
+            }
+            return of(new AuthActions.LoginFail(errorMessage));
           })
         );
+    })
+  );
+
+  @Effect({ dispatch: false })
+  authSuccess = this.actions$.pipe(
+    // After successful login
+    ofType(AuthActions.LOGIN),
+    tap(() => {
+      this.router.navigate(['/']);
     })
   );
   constructor(
     private actions$: Actions,
     private http: HttpClient,
-    private authKeyService: AuthKeyService
+    private authKeyService: AuthKeyService,
+    private router: Router
   ) {}
 }
